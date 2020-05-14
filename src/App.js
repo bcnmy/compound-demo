b95547c9bb877ceb334e9632b58057cd7955e8a1
Anubhav <anubhavgirdhar@gmail.com>
2020-05-14 21:39:03 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,91 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import logo from './logo.svg';
 import './App.css';
+import Web3 from 'web3'
+import Biconomy from "@biconomy/mexa";
+const { config } = require("./config");
 
+const domainType = [
+  { name: "name", type: "string" },
+  { name: "version", type: "string" },
+  { name: "chainId", type: "uint256" },
+  { name: "verifyingContract", type: "address" }
+];
+
+const metaTransactionType = [
+  { name: "holder", type: "address" },
+  { name: "nonce", type: "uint256" }
+];
+
+let domainData = {
+  name: "Compound-DApp",
+  version: "1",
+  chainId: "42", // Kovan
+  verifyingContract: config.contract.address
+};
+
+let web3;
+let contract;
+let biconomy;
 function App() {
+
+  useEffect(() => {
+    if (!window.ethereum) {
+      alert("Metamask is required to use this DApp")
+      return;
+    }
+    biconomy = new Biconomy(window.ethereum, { apiKey: "GwiBC__tc.8c503ac0-dc5f-4e57-a738-fb44ca54847c" });
+    web3 = new Web3(biconomy);
+    biconomy.onEvent(biconomy.READY, async () => {
+      // Initialize your dapp here like getting user accounts etc
+      await window.ethereum.enable();
+      contract = new web3.eth.Contract(config.contract.abi, config.contract.address);
+      startApp();
+    }).onEvent(biconomy.ERROR, (error, message) => {
+      // Handle error while initializing mexa
+      console.log(error)
+    });
+  }, []);
+
+  async function startApp() {
+    const nonce = await contract.methods.nonces(window.ethereum.selectedAddress).call();
+    let message = {
+      holder: window.ethereum.selectedAddress,
+      nonce: parseInt(nonce),
+    };
+
+    const dataToSign = JSON.stringify({
+      types: {
+        EIP712Domain: domainType,
+        MetaTransaction: metaTransactionType
+      },
+      domain: domainData,
+      primaryType: "MetaTransaction",
+      message: message
+    });
+
+    window.web3.currentProvider.sendAsync(
+      {
+        jsonrpc: "2.0",
+        id: 999999999999,
+        method: "eth_signTypedData_v4",
+        params: [window.ethereum.selectedAddress, dataToSign]
+      },
+      function (err, result) {
+        if (err) {
+          return console.error(err);
+        }
+        const signature = result.result.substring(2);
+        const r = "0x" + signature.substring(0, 64);
+        const s = "0x" + signature.substring(64, 128);
+        const v = parseInt(signature.substring(128, 130), 16);
+        (async function withdraw() {
+          const result = await contract.methods.withdraw(window.ethereum.selectedAddress, nonce, r, s, v).send({ from: window.ethereum.selectedAddress })
+          console.log(result)
+        })();
+      }
+    );
+  }
   return (
     <div className="App">
       <header className="App-header">
---
+++ src/config.js
@@ -0,0 +1,74 @@
+let config = {};
+config.contract = {
+    address: "0x23a950252a89e46c5aB63e8F7906974F5d3d0330",
+    abi: [
+        {
+            "constant": false,
+            "inputs": [],
+            "name": "deposit",
+            "outputs": [],
+            "payable": true,
+            "stateMutability": "payable",
+            "type": "function"
+        },
+        {
+            "constant": false,
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "userAddress",
+                    "type": "address"
+                },
+                {
+                    "internalType": "uint256",
+                    "name": "nonce",
+                    "type": "uint256"
+                },
+                {
+                    "internalType": "bytes32",
+                    "name": "r",
+                    "type": "bytes32"
+                },
+                {
+                    "internalType": "bytes32",
+                    "name": "s",
+                    "type": "bytes32"
+                },
+                {
+                    "internalType": "uint8",
+                    "name": "v",
+                    "type": "uint8"
+                }
+            ],
+            "name": "withdraw",
+            "outputs": [],
+            "payable": false,
+            "stateMutability": "nonpayable",
+            "type": "function"
+        },
+        {
+            "constant": true,
+            "inputs": [
+                {
+                    "internalType": "address",
+                    "name": "",
+                    "type": "address"
+                }
+            ],
+            "name": "nonces",
+            "outputs": [
+                {
+                    "internalType": "uint256",
+                    "name": "",
+                    "type": "uint256"
+                }
+            ],
+            "payable": false,
+            "stateMutability": "view",
+            "type": "function"
+        }
+    ]
+}
+
+
+module.exports = { config }
